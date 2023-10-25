49c70999cf508509451931316b5b8f0b18406ecc
JavaBorka <borka34@gmail.com>
2023-10-25 21:16:25 +0200

    import zadání

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import Rate from './components/Rate';
+import './App.css'
+
+const App = () => {
+  const [currency, setCurrency] = useState('USD');
+
+  const handleCurrencyChange = (e) => {
+    setCurrency(e.target.value);
+  };
+
+  return (
+    <div className="container">
+      <h1>Kurzovní lístek</h1>
+      <div className="currency-info">
+        <form>
+          <div className="form-field">
+            <label htmlFor="currency-select">Měna:</label>
+            <select
+              id="currency-select"
+              value={currency}
+              onChange={handleCurrencyChange}
+            >
+              <option value="USD">Americký dolar</option>
+              <option value="EUR">Euro</option>
+              <option value="GBP">Britská libra</option>
+            </select>
+          </div>
+        </form>
+        <Rate />
+      </div>
+    </div>
+  );
+};
+
+export default App
